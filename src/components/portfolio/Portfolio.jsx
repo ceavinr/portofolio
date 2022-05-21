b465a48c535768c860fc3d822cc40b230d40741c
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-05-21 15:26:20 +0700

    update portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio/portfolio1.jpg";
+import IMG1 from "../../assets/portfolio/portfolio1.png";
 import IMG2 from "../../assets/portfolio/portfolio2.png";
+import IMG3 from "../../assets/portfolio/portfolio3.png";
 import { GiPlayButton } from "react-icons/gi";
 
 const data = [
@@ -10,6 +11,7 @@ const data = [
     image: IMG1,
     title: "MovieHub",
     tech: "React",
+    role: "Frontend Developer",
     github: "https://github.com/ceavinr/movie-hub",
     demo: "https://movie-hub-website.netlify.app/",
   },
@@ -18,8 +20,18 @@ const data = [
     image: IMG2,
     title: "My Portofolio Website",
     tech: "React",
+    role: "Frontend Developer",
     github: "https://github.com/ceavinr/portofolio",
     demo: "https://ceavinr.netlify.app/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Montirloka",
+    tech: "React, Firebase",
+    role: "Full Stack Developer",
+    github: "https://github.com/ceavinr/montirloka",
+    demo: "https://montirloka-457b6.web.app/",
   },
 ];
 
@@ -30,13 +42,14 @@ const portofolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, tech, github, demo }) => {
+        {data.map(({ id, image, title, tech, role, github, demo }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+              <h4>{role}</h4>
               <h5>{tech}</h5>
               <div className="portfolio__item-cta">
                 {/* <a href={github} className="btn" target="_blank">
