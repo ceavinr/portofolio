e7c9324b8b2089c3b9b4021d90439bd648caee65
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-13 18:46:40 +0700

    add portfolio page

---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../portfolio/portfolio.css";
+import Portfolio from "../portfolio/Portfolio";
+
+const Projects = () => {
+  return <Portfolio />;
+};
+
+export default Projects;
