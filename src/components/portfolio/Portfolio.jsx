59bb9266c90d108b72163abdf1f04ac45c6805a0
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-06-13 18:50:45 +0700

    see more button

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,9 @@ import "./portfolio.css";
 import IMG1 from "../../assets/portfolio/portfolio1.png";
 import IMG2 from "../../assets/portfolio/portfolio2.png";
 import IMG3 from "../../assets/portfolio/portfolio3.png";
+import IMG4 from "../../assets/portfolio/portfolio4.png";
 import { GiPlayButton } from "react-icons/gi";
+import { BsChevronCompactDown, BsChevronDoubleDown } from "react-icons/bs";
 
 const data = [
   {
@@ -33,38 +35,63 @@ const data = [
     github: "https://github.com/ceavinr/montirloka",
     demo: "https://montirloka-457b6.web.app/",
   },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Simple To-do-list",
+    tech: "Vanilla Javascript",
+    role: "Frontend Developer",
+    github: "https://github.com/ceavinr/to-do-list",
+    demo: "https://ceavinr.github.io/to-do-list/",
+  },
 ];
 
-const portofolio = () => {
+const Portfolio = ({ slice = data.length }) => {
   return (
     <section id="portfolio" data-aos="fade-up">
       <h5>My recent projects</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
-        {data.map(({ id, image, title, tech, role, github, demo }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-              </div>
-              <h3>{title}</h3>
-              <h4>{role}</h4>
-              <h5>{tech}</h5>
-              <div className="portfolio__item-cta">
-                {/* <a href={github} className="btn" target="_blank">
+      <div className="container ">
+        <div className="portfolio__container">
+          {data
+            .slice(0, slice)
+            .map(({ id, image, title, tech, role, github, demo }) => {
+              return (
+                <article key={id} className="portfolio__item">
+                  <div className="portfolio__item-image">
+                    <img src={image} alt={title} />
+                  </div>
+                  <h3>{title}</h3>
+                  <h4>{role}</h4>
+                  <h5>{tech}</h5>
+                  <div className="portfolio__item-cta">
+                    {/* <a href={github} className="btn" target="_blank">
                   GitHub
                 </a> */}
-                <a href={demo} className="btn btn-primary" target="_blank">
-                  <GiPlayButton className="portofolio__item-icon" /> Live Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
+                    <a href={demo} className="btn btn-primary" target="_blank">
+                      <GiPlayButton className="portofolio__item-icon" />
+                      {"\u00a0"} Live Demo
+                    </a>
+                  </div>
+                </article>
+              );
+            })}
+        </div>
+        <div className="see__more">
+          {slice <= 3 ? (
+            <a href="/portfolio">
+              Show More
+              <BsChevronCompactDown className="chevron__icon" />
+              <BsChevronDoubleDown className="double-chevron__icon" />
+            </a>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </section>
   );
 };
 
-export default portofolio;
+export default Portfolio;
