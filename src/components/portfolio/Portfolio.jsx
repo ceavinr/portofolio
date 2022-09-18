7e7c35008db156b7fef1471ece4a3ad4d0714eea
ceavinr <16521304@std.stei.itb.ac.id>
2022-09-18 10:52:46 +0700

    portfolio update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,71 +1,10 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio/portfolio1.png";
-import IMG2 from "../../assets/portfolio/portfolio2.png";
-import IMG3 from "../../assets/portfolio/portfolio3.png";
-import IMG4 from "../../assets/portfolio/portfolio4.png";
-import IMG5 from "../../assets/portfolio/portfolio5.png";
-import IMG6 from "../../assets/portfolio/portfolio6.png";
+
 import { GiPlayButton } from "react-icons/gi";
 import { FaGithub } from "react-icons/fa";
 import { BsChevronCompactDown, BsChevronDoubleDown } from "react-icons/bs";
-
-const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: "MovieHub",
-    tech: "React",
-    role: "Frontend Developer",
-    github: "https://github.com/ceavinr/movie-hub",
-    demo: "https://movie-hub-website.netlify.app/",
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: "My Portfolio Website",
-    tech: "React",
-    role: "Frontend Developer",
-    github: "https://github.com/ceavinr/portofolio",
-    demo: "https://ceavinr.netlify.app/",
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: "StudyMate",
-    tech: "React, MongoDB, Express.js",
-    role: "Full Stack Developer",
-    github: "https://github.com/ceavinr/Milestone02_K05",
-    demo: "https://studymate.vercel.app/",
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: "Colettes",
-    tech: "React",
-    role: "Frontend Developer",
-    github: "https://github.com/ceavinr/colettes",
-    demo: "https://colettes.netlify.app/",
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: "Montirloka",
-    tech: "React, Firebase",
-    role: "Full Stack Developer",
-    github: "https://github.com/ceavinr/montirloka",
-    demo: "https://montirloka-457b6.web.app/",
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: "Simple To-do-list",
-    tech: "Vanilla Javascript",
-    role: "Frontend Developer",
-    github: "https://github.com/ceavinr/to-do-list",
-    demo: "https://ceavinr.github.io/to-do-list/",
-  },
-];
+import { data } from "./db";
 
 const Portfolio = ({ slice = data.length }) => {
   return (
---
+++ src/components/portfolio/db.js
@@ -0,0 +1,54 @@
+import IMG1 from "../../assets/portfolio/portfolio1.png";
+import IMG2 from "../../assets/portfolio/portfolio2.png";
+import IMG3 from "../../assets/portfolio/portfolio3.png";
+import IMG4 from "../../assets/portfolio/portfolio4.png";
+// import IMG5 from "../../assets/portfolio/portfolio5.png";
+import IMG6 from "../../assets/portfolio/portfolio6.png";
+
+export const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "MovieHub",
+    tech: "React",
+    role: "Frontend Developer",
+    github: "https://github.com/ceavinr/movie-hub",
+    demo: "https://movie-hub-website.netlify.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "My Portfolio Website",
+    tech: "React",
+    role: "Frontend Developer",
+    github: "https://github.com/ceavinr/portofolio",
+    demo: "https://ceavinr.netlify.app/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "StudyMate",
+    tech: "React, MongoDB, Express.js",
+    role: "Full Stack Developer",
+    github: "https://github.com/ceavinr/Milestone02_K05",
+    demo: "https://studymate.vercel.app/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Colettes",
+    tech: "React",
+    role: "Frontend Developer",
+    github: "https://github.com/ceavinr/colettes",
+    demo: "https://colettes.netlify.app/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Simple To-do-list",
+    tech: "Vanilla Javascript",
+    role: "Frontend Developer",
+    github: "https://github.com/ceavinr/to-do-list",
+    demo: "https://ceavinr.github.io/to-do-list/",
+  },
+];
