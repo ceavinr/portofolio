21dc82fa472cf9bcfb62b7939b4b1ebed6b4f675
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-12-01 01:17:39 +0700

    remove cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import CV from "../../assets/ceavinrufus-cv.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download CV
+      <a href="#portfolio" className="btn">
+        My Projects
       </a>
       <a href="#contact" className="btn btn-primary">
-        Contact Me
+        Contact Me!
       </a>
     </div>
   );
