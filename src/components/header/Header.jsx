23a6b51841882f45d3e15ac2465422c53c092b5c
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-12-10 09:51:01 +0700

    feat: typewriter

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,15 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
 import Socials from "./Socials";
+import { useTypingEffect } from "../../hooks/typing-effect";
 
 const Header = () => {
+  const text = useTypingEffect("Ceavin Rufus", 100);
   return (
     <header>
       <div className="container header__container" data-aos="fade-up">
         <h5>Hello I'm</h5>
-        <h1>Ceavin Rufus</h1>
+        <h1>{text}</h1>
         <h5 className="text-light">Student at Institut Teknologi Bandung</h5>
         <CTA />
         <Socials />
